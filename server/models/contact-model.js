46c3dffeec2d4086920b01c25ce7fdf7f192a892
Yashwadkar2121 <yashwadkar132@gmail.com>
2024-03-20 00:24:48 +0530

    Contact From backend Okey

---
+++ server/models/contact-model.js
@@ -0,0 +1,20 @@
+// destructor in mongoose
+const { Schema, model } = require("mongoose");
+
+const contactschema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+});
+
+const Contact = new model("Contact", contactschema);
+module.exports = Contact;
--- server/server.js
+++ server/server.js
@@ -3,15 +3,17 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 5000;
-const router = require("./router/auth-rother");
+const authRoute = require("./router/auth-rother");
 const connectDB = require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
+const contactRoute = require("./router/contact-router");
 
 // This line of code adds Express middleware that  parses incoming request bodies with JSON payloads. It's important to place this before any routes that need  to handle JSON data in the request body. This middleware is responsible for parsing JSON data from requests, and it should be applied at the beginning of your middleware stack to ensure it's available for all subsequest routs handlers.
 app.use(express.json());
 
 // Mount the Router : To use the router in your main Express app,you can "mount" it at a specific URL prefix
-app.use("/api/auth", router);
+app.use("/api/auth", authRoute);
+app.use("/api/from", contactRoute);
 
 // For use to errorMiddleware
 app.use(errorMiddleware);
